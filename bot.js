d65c77c227c0dbcfc8cb5a3d1e0d2a50c20812fe
Rob Erskine <Rob@RobErskine.com>
2019-09-03 15:55:33 -0400

    replacing exec for spawn

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var Twit = require('twit');
+const spawn = require('child_process').spawn;
 
 var T = new Twit(require('./config.js'));
 
@@ -11,13 +12,10 @@ var T = new Twit(require('./config.js'));
 // }
 
 function cowsayPLZ() {
-    var exec = require('child_process').exec;
-    var fortuneCmd = 'fortune -s -n 220';
+    var fortuneCmd = spawn('fortune', ['-s', '-n 180']);
 
-    exec(fortuneCmd, function(error, stdout, stderr) {
-      var response = stdout;
-      console.log(response);
-      T.post('statuses/update', { status: response +"----------\r\n    \\  ^__^\r\n     \\  (oo)\\__\r\n        (__)\\ - "+ Math.floor(Math.random() * 999999) }, function(err, data, response) {
+    fortuneCmd.stdout.on('data', (data) => {
+      T.post('statuses/update', { status: data +"----\r\n\  \\   \r\n\  ^__^\r\n\  (oo)\_________\r\n\   (__)\                )\/\\r\n\         ||----w ||\r\n\         ||          ||"}, function(err, data, response) {
         console.log(data)
       });
     });
